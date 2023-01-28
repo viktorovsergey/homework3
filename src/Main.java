5f5d949bbb7450fc21858b477b026bad188bb76a
ya_zabiyaka <ya_zabiyaka@bk.ru>
2023-01-28 20:27:47 +0300

    Задание 1 решено

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+     /* Задача 1
+        переменной dog значение 8.0;
+        переменной cat значение 3.6;
+        переменной paper значение 763789
+        Выведите значения каждой переменной в консоль.
+        * */
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763_789;
+        System.out.println("dog = " + dog);
+        System.out.println("cat = " + cat);
+        System.out.println("paper = " + paper);
+        System.out.println(" ");
     }
 }
